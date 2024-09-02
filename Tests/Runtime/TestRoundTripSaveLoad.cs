9ee4ef07215e71f01924713a36078eab75afe66a
Dan Miller <dan.miller@fraculation.com>
2024-09-02 00:01:27 -0500

    add TODO and variant test around a failing, potential bugged, test

--- Tests/Runtime/TestRoundTripSaveLoad.cs
+++ Tests/Runtime/TestRoundTripSaveLoad.cs
@@ -52,6 +52,40 @@ namespace Buck.SaveAsync.Tests
                 Assert.AreEqual("Hello, World!", saveable.CurrentState);
             });
         
+        class SaveObjectWithNestedVector3
+        {
+            public Vector3 NestedVector3 { get; set; }
+        }
+        
+        [UnityTest]
+        public IEnumerator TestSaveSystem_WhenSavesVector3StateInNestedProperty_AndChangesState_RestoresState() 
+            => AsyncToCoroutine.AsCoroutine(async () => 
+            {
+                // Arrange
+                var seed = Guid.NewGuid().ToString();
+                
+                var fileHandler = CreateFileHandler();
+                SetupSaveManager(fileHandler);
+                var saveable = CreateSaveableEntity("saveable_" + seed, "test.dat");
+                
+                var expected = new Vector3(1, 2.3f, 10000.2f);
+                saveable.CurrentState = new SaveObjectWithNestedVector3
+                {
+                    NestedVector3 = expected
+                };
+                await SaveManager.Save("test.dat");
+
+                // Act
+                saveable.CurrentState = Vector3.zero;
+                await SaveManager.Load("test.dat");
+            
+                // Assert
+                var actual = (SaveObjectWithNestedVector3)saveable.CurrentState;
+                Assert.AreEqual(0, (expected - actual.NestedVector3).magnitude, 0.0001f);
+            });
+        
+        
+        // TODO: this test passes only when -not- using the Json-for-Unity Vector3 converter 
         [UnityTest]
         public IEnumerator TestSaveSystem_WhenSavesVector3State_AndChangesState_RestoresState() 
             => AsyncToCoroutine.AsCoroutine(async () => 
