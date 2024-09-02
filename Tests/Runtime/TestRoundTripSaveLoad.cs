9a3ef020f189ab57225a72c21299a345caa99114
Dan Miller <dan.miller@fraculation.com>
2024-09-01 23:16:39 -0500

    add test for Vector3 serialization. seems to work

--- Tests/Runtime/TestRoundTripSaveLoad.cs
+++ Tests/Runtime/TestRoundTripSaveLoad.cs
@@ -9,7 +9,7 @@ namespace Buck.SaveAsync.Tests
     public class TestRoundTripSaveLoad : TestCaseBase
     {
         [UnityTest]
-        public IEnumerator TestSaveSystem_WhenSavesState_AndChangesState_RestoresState() 
+        public IEnumerator TestSaveSystem_WhenSavesStringState_AndChangesState_RestoresState() 
             => AsyncToCoroutine.AsCoroutine(async () => 
             {
                 // Arrange
@@ -31,7 +31,7 @@ namespace Buck.SaveAsync.Tests
             });
         
         [UnityTest]
-        public IEnumerator TestSaveSystem_WhenSavesState_AndChangesState_RestoresState_WithDelay() 
+        public IEnumerator TestSaveSystem_WhenSavesStringState_AndChangesState_RestoresState_WithDelay() 
             => AsyncToCoroutine.AsCoroutine(async () => 
             {
                 // Arrange
@@ -50,6 +50,30 @@ namespace Buck.SaveAsync.Tests
             
                 // Assert
                 Assert.AreEqual("Hello, World!", saveable.CurrentState);
+            });
+        
+        [UnityTest]
+        public IEnumerator TestSaveSystem_WhenSavesVector3State_AndChangesState_RestoresState() 
+            => AsyncToCoroutine.AsCoroutine(async () => 
+            {
+                // Arrange
+                var seed = Guid.NewGuid().ToString();
+                
+                var fileHandler = CreateFileHandler();
+                SetupSaveManager(fileHandler);
+                var saveable = CreateSaveableEntity("saveable_" + seed, "test.dat");
+                
+                var expected = new Vector3(1, 2.3f, 10000.2f);
+                saveable.CurrentState = expected;
+                await SaveManager.Save("test.dat");
+
+                // Act
+                saveable.CurrentState = Vector3.zero;
+                await SaveManager.Load("test.dat");
+            
+                // Assert
+                var actual = (Vector3)saveable.CurrentState;
+                Assert.AreEqual(0, (expected - actual).magnitude, 0.0001f);
             });
     }
 }
